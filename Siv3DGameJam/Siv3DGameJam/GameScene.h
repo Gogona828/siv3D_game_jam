be372a446d163d53c78a8d985f1fc44f81a03c87
UchinoKoki <87020118+UchinoKoki@users.noreply.github.com>
2025-10-26 18:48:33 +0900

    SceneManagerの作成

---
+++ Siv3DGameJam/Siv3DGameJam/GameScene.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+# include <Siv3D.hpp>
+using App = s3d::SceneManager<s3d::String, void>;
+
+class GameScene : public App::Scene
+{
+public:
+	GameScene(const InitData& init) : IScene(init) {}
+
+	void update() override
+	{
+		if (s3d::SimpleGUI::Button(U"タイトルへ", { 300, 300 })) {
+			changeScene(U"Title");
+		}
+	}
+
+	void draw() const override
+	{
+		s3d::Scene::SetBackground(s3d::Palette::Gray);
+		s3d::FontAsset(U"Game")(U"Game Scene").drawAt({ 60, 40 });
+	}
+};
---
+++ Siv3DGameJam/Siv3DGameJam/TitleScene.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+# include <Siv3D.hpp>
+using App = s3d::SceneManager<s3d::String, void>;
+
+class TitleScene : public App::Scene
+{
+public:
+	TitleScene(const InitData& init) : IScene(init) {}
+
+	void update() override
+	{
+		if (s3d::SimpleGUI::Button(U"ゲーム開始", { 300, 300 })) {
+			changeScene(U"Game"); // 公式の changeScene
+		}
+	}
+
+	void draw() const override
+	{
+		s3d::Scene::SetBackground(s3d::Palette::Skyblue);
+		s3d::FontAsset(U"Title")(U"Title Scene").drawAt(s3d::Scene::Center());
+	}
+};
